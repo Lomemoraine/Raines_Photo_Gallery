69e25aa0faeb3cf31f945c604c691667e50e7164
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-05-29 12:43:08 +0300

    write fisrst test for category model and run it-it passed

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import Category, Location, Photo
 # Create your tests here.
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.category = Category(name='CLOSE_UP')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
