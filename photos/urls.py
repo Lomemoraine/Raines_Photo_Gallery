fbb850080072d6881d1654930b73806cd762f2a2
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-05-29 21:15:34 +0300

    display photos filterng by category and location

--- photos/urls.py
+++ photos/urls.py
@@ -6,9 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.gallery, name='gallery-home'),
-    # path(r'^search/',views.search_results, name='search_results'),
-    # re_path(r'^category/(\w+)', views.get_category,name='get_category'),
-    # re_path(r'^location/(\w+)', views.get_location,name='get_location'),
+    re_path(r'^location/(\w+)', views.get_location,name='get_location'),
+    re_path(r'category/(\w+)', views.get_category,name='get_category'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -8,3 +8,14 @@ def gallery(request):
     category_results = Category.objects.all()
     location_results = Location.objects.all()
     return render(request, 'index.html', {'all_images':all_images,'location_results':location_results,'category_results':category_results})
+
+def get_category(request,category):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    category_result = Photo.objects.filter(image_category__cat_name = category)
+    return render(request,'index.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
+def get_location(request,location):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    location_result = Photo.objects.filter(image_location__location= location)
+    return render(request,'index.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
